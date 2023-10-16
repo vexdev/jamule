61ebf705a5fe3d0e843efb956192b005aa4a89d8
Luca Vitucci <luca@vexdev.com>
2023-10-16 23:27:10 +0200

    Implement special search status codes

--- lib/src/main/kotlin/jamule/response/SearchStatusResponse.kt
+++ lib/src/main/kotlin/jamule/response/SearchStatusResponse.kt
@@ -6,7 +6,11 @@ import jamule.ec.packet.Packet
 data class SearchStatusResponse(val status: Float) : Response {
     companion object {
         internal fun fromPacket(packet: Packet) = SearchStatusResponse(
-            packet.numeric(ECTagName.EC_TAG_SEARCH_STATUS)!!.getNumber().div(100f)
+            packet.numeric(ECTagName.EC_TAG_SEARCH_STATUS)!!.getNumber().let { num ->
+                // Local searches always return 0xFFFF
+                // Kad searches return 0xFFFE when they are finished, otherwise 0
+                if (num == 0xFFFF || num == 0xFFFE) 1f else num / 100f
+            }
         )
     }
 }
