7aa8ff4dd2e0f85663e15286feb30ab83cb178a0
Luca Vitucci <luca@vexdev.com>
2026-05-06 08:21:43 +0200

    fix: point Sonatype publishing to Central staging API

--- build.gradle.kts
+++ build.gradle.kts
@@ -102,6 +102,9 @@ publishing {
 
 nexusPublishing {
     repositories {
-        sonatype()
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+        }
     }
 }
